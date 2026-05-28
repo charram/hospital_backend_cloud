576cc8946f34cf259570380ad0f47e8919214064
charram <charampin2633@gmail.com>
2026-05-28 21:17:32 +0700

    fix render postgres

--- db_connect.php
+++ db_connect.php
@@ -1,31 +1,22 @@
-
 <?php
+
 header("Content-Type: application/json; charset=utf-8");
 
-$host = "metro.proxy.rlwy.net";
-$port = "19442";
+$host   = "metro.proxy.rlwy.net";
+$port   = "19442";
 $dbname = "railway";
-$user = "postgres";
+$user   = "postgres";
 $pass = "vCiFHpFBjjvvEGIqNILhSUeCIyJTKmxq";
+$conn = pg_connect(
+    "host=$host port=$port dbname=$dbname user=$user password=$pass sslmode=require"
+);
 
-try {
-    $pdo = new PDO(
-        "pgsql:host=$host;port=$port;dbname=$dbname;sslmode=require",
-        $user,
-        $pass,
-        [
-            PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
-        ]
-    );
+if (!$conn) {
 
-    // compatibility
-    $conn = $pdo;
-
-} catch (PDOException $e) {
     echo json_encode([
         "success" => false,
-        "message" => "Database connection failed",
-        "error" => $e->getMessage()
+        "message" => "Database connection failed"
     ]);
+
     exit;
 }
