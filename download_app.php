4c4122436a44269bae531785d7087772f7ee861f
charram <charampin2633@gmail.com>
2026-05-29 13:36:10 +0700

    fix download

--- download_app.php
+++ download_app.php
@@ -1,5 +1,5 @@
-```php
 <?php
+
 $file = __DIR__ . '/app-release.apk';
 
 if (!file_exists($file)) {
@@ -7,13 +7,12 @@ if (!file_exists($file)) {
     exit('APK not found');
 }
 
-// ล้าง output buffer กัน header พัง
 if (ob_get_level()) {
     ob_end_clean();
 }
 
 header('Content-Description: File Transfer');
-header('Content-Type: application/octet-stream');
+header('Content-Type: application/vnd.android.package-archive');
 header('Content-Disposition: attachment; filename="OpenHospital-v1.apk"');
 header('Content-Transfer-Encoding: binary');
 header('Expires: 0');
@@ -21,8 +20,7 @@ header('Cache-Control: must-revalidate');
 header('Pragma: public');
 header('Content-Length: ' . filesize($file));
 
-flush();
 readfile($file);
 exit;
+
 ?>
-```
