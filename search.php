cba7fcf131b061e7d3b926239fa814ec5ea73d7e
Charampin <charampin2633@gmail.com>
2026-07-02 22:23:03 +0700

    Fix disease search

--- search.php
+++ search.php
@@ -13,7 +13,7 @@ $result = [
     "products"  => [],
 ];
 
-if ($q !== "") {
+if ($q != "") {
 
     $keyword = "%" . $q . "%";
 
@@ -36,11 +36,7 @@ if ($q !== "") {
     LIMIT 10
     ";
 
-    $res1 = pg_query_params(
-        $conn,
-        $sql1,
-        [$keyword]
-    );
+    $res1 = pg_query_params($conn, $sql1, [$keyword]);
 
     while ($row = pg_fetch_assoc($res1)) {
         $result["hospitals"][] = $row;
@@ -86,72 +82,19 @@ if ($q !== "") {
             OR meta::text ILIKE $1
         )
 
-    UNION ALL
-
-    SELECT
-        'lung' AS category,
-        hospital_id,
-        title,
-        description,
-        image_path,
-        upload_type
-    FROM lung_center
-    WHERE
-        upload_type='disease'
-        AND (
-            title ILIKE $1
-            OR description ILIKE $1
-            OR meta::text ILIKE $1
-        )
-
-    UNION ALL
-
-    SELECT
-        'heart' AS category,
-        hospital_id,
-        title,
-        description,
-        image_path,
-        upload_type
-    FROM heart_center
-    WHERE
-        upload_type='disease'
-        AND (
-            title ILIKE $1
-            OR description ILIKE $1
-            OR meta::text ILIKE $1
-        )
-
-    UNION ALL
-
-    SELECT
-        'kidney' AS category,
-        hospital_id,
-        title,
-        description,
-        image_path,
-        upload_type
-    FROM kidney_center
-    WHERE
-        upload_type='disease'
-        AND (
-            title ILIKE $1
-            OR description ILIKE $1
-            OR meta::text ILIKE $1
-        )
-
+    ORDER BY title
     LIMIT 20
 
     ";
 
-    $res2 = pg_query_params(
-        $conn,
-        $sql2,
-        [$keyword]
-    );
+    $res2 = pg_query_params($conn, $sql2, [$keyword]);
 
-    while ($row = pg_fetch_assoc($res2)) {
-        $result["diseases"][] = $row;
+    if ($res2) {
+
+        while ($row = pg_fetch_assoc($res2)) {
+            $result["diseases"][] = $row;
+        }
+
     }
 
     // ==========================================
@@ -172,15 +115,12 @@ if ($q !== "") {
     LIMIT 10
     ";
 
-    $res3 = pg_query_params(
-        $conn,
-        $sql3,
-        [$keyword]
-    );
+    $res3 = pg_query_params($conn, $sql3, [$keyword]);
 
     while ($row = pg_fetch_assoc($res3)) {
         $result["products"][] = $row;
     }
+
 }
 
 echo json_encode(
